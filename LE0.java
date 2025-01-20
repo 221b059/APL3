74c591a817d2042abbc4153c95c18bcf66ba3884
221B059 <221B059@juet.co.in>
2025-01-20 15:41:28 +0530

    Lab Execise

---
+++ LE0.java
@@ -0,0 +1,26 @@
+public class LE0
+{
+    	 String name;
+	     int speed;
+	     
+	     void setSpeed(){
+	         this.speed=speed;
+	     }
+	     void setName(){
+	         this.name=name;
+	     }
+	     void getSpeed(){
+	         return this.speed;
+	     }
+	     
+	     void getName(){
+	         return this.name;
+	     }
+	public static void main(String[] args) {
+	    
+	    Main m1=new Main();
+	    
+	    
+	 }
+}
+
